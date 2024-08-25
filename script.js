3378517ee797ea9e0c7e3afb80d71f72d4b9b6ca
Eric <176455136+bonShonjon@users.noreply.github.com>
2024-08-25 17:42:48 +1000

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-function getComputerChoice () {
-  //Create variable called randomNumber and assign it to a random number between 0 and 3
+function getComputerChoice() {
+  //Generate random number between 0 and 3
   let randomNumber = Math.random() * 3;
-  //Create output variable called computerChoice
   let computerChoice;
 
   //randomNumber determines computer choice
   if (randomNumber >= 0 && randomNumber < 1) {
-    computerChoice = "Rock";
+    computerChoice = "rock";
   } else if (randomNumber >= 1 && randomNumber < 2) {
-    computerChoice = "Paper";
+    computerChoice = "paper";
   } else if (randomNumber >= 2 && randomNumber<= 3) {
-    computerChoice = "Scissors";
+    computerChoice = "scissors";
   }
 
   return computerChoice;
+}
+
+function getHumanChoice() {
+  //Get player's input and convert to lowercase
+  let playerInput = prompt("Which do you choose: Rock, Paper or Scissors?").toLowerCase();
+  let playerChoice;
+  
+  //check player's input is valid, otherwise alert and recursively call this function again
+  if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
+    playerChoice = playerInput;
+  } else {
+    alert("Your choice is invalid. Please try again.")
+    playerChoice = getHumanChoice();
+  }
+
+  return playerChoice;
 }
